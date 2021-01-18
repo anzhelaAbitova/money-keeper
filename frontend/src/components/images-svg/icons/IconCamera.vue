0028312f89f42807fee713d6aba753471d98cb56
Talai Sergei <tsa@tut.by>
2021-01-18 23:25:06 +0300

    feat: add cabinet base layout

---
+++ frontend/src/components/images-svg/icons/IconCamera.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.5 8a4.5 4.5 0 110 9 4.5 4.5 0 010-9zm0 1a3.5 3.5 0 100 7 3.5 3.5 0 000-7zM5 5h2l2-2h5l2 2h2a3 3 0 013 3v9a3 3 0 01-3 3H5a3 3 0 01-3-3V8a3 3 0 013-3zm4.414-1l-2 2H5a2 2 0 00-2 2v9a2 2 0 002 2h13a2 2 0 002-2V8a2 2 0 00-2-2h-2.414l-2-2H9.414z" fill="#E6F3F5"/></svg>
+</template>
---
+++ frontend/src/views/cabinet/Goals.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="cabinet">
+    <h1>Goals</h1>
+  </div>
+</template>
