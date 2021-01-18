017a6ba0e4a69b7ceff6ea0324f724ec31c44d00
Talai Sergei <tsa@tut.by>
2021-01-18 19:36:33 +0300

    feat: add home page (desktop)

--- frontend/src/layouts/PageLayout.vue
+++ frontend/src/layouts/PageLayout.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="app-page">
-    <slot/>
+    <div class="app-page__container">
+      <div class="app-page__header">
+        <nav class="app-page__links">
+          <a href="#features" class="app-page__link">Features</a>
+          <a href="#" class="app-page__link">Our team</a>
+          <a href="#contacts" class="app-page__link">Contacts</a>
+        </nav>
+        <div class="app-page__auth">
+          <div class="app-page__auth-reg">
+            <a href="javascript:void(0);" @click="$router.push('/cabinet/home')">
+              <user-image></user-image>
+            </a>
+          </div>
+        </div>
+      </div>
+      <slot/>
+      <div class="app-page__footer">
+        <div class="app-page__footer-container">
+          <!-- <div class="app-page__footer-logo">
+            <img src="@/assets/images/rslogo.svg" alt="">
+          </div> -->
+          <div class="app-page__footer-year">
+            2021 &copy;
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import UserImage from '@/components/images-svg/UserImage.vue';
 
-@Component
+@Component({
+  components: {
+    UserImage,
+  },
+})
 export default class PageLayout extends Vue {}
 </script>
--- frontend/src/views/pages/Home.vue
+++ frontend/src/views/pages/Home.vue
@@ -1,28 +1,90 @@
 <template>
-  <div class="home">
-    <div>
-      <img alt="Vue logo" src="../../assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-page__main">
+    <div class="app-page__banner">
+      <div class="app-page__banner-col">
+        <div class="app-page__banner-content">
+          <div class="app-page__banner-title">
+            <h1>App developed for<br>small business</h1>
+          </div>
+          <div class="app-page__banner-text">
+            Our team build this app especially for small business. This app is free, use to use,
+             doesn't need to download, can be used on desktop and smartphones alike.
+          </div>
+          <div class="app-page__banner-button">
+            <button class="btn btn-primery" @click="$router.push('/cabinet/home')">Use now</button>
+          </div>
+        </div>
+      </div>
+      <div class="app-page__banner-col">
+        <div class="app-page__banner-image">
+          <BannerImage />
+        </div>
+      </div>
     </div>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/cabinet/home">Cabinet</router-link>
-    </div> -->
+    <div class="app-page__description" id="features">
+      <div class="app-page__description-title">
+        <h2>Features</h2>
+      </div>
+      <div class="app-page__description-cards">
+        <template v-for="(card, i) in featuresData">
+          <FeaturesCard :dataCard="card" :key="'featurescard-' + i"/>
+        </template>
+      </div>
+    </div>
+    <div class="app-page__contacts" id="contacts">
+      <div class="app-page__contacts-title">
+        <h2>Our contacts</h2>
+      </div>
+      <div class="app-page__contacts-content">
+        <div class="app-page__contacts-item">
+          <a href="https://github.com/tsaminsk" class="app-page__contacts-link" target="_blank">Sergei Talai</a>
+        </div>
+        <div class="app-page__contacts-item">
+          <a href="https://rs.school/index.html" class="app-page__contacts-link" target="_blank">
+            <img src="@/assets/images/rslogo.svg" alt="">
+          </a>
+        </div>
+        <div class="app-page__contacts-item">
+          <a href="https://github.com/anzhelaAbitova" class="app-page__contacts-link" target="_blank">Anzhela Abitova</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
+import BannerImage from '@/components/images-svg/BannerImage.vue';
+
+import FeaturesCard from '@/components/page/FeaturesCard.vue';
+import { IFeaturesCard } from '@/types';
 
 @Component({
   components: {
     HelloWorld,
+    BannerImage,
+    FeaturesCard,
   },
 })
 
 export default class Home extends Vue {
-  // private isComponentModalActive = false;
+  featuresData: IFeaturesCard[] = [
+    {
+      title: 'No download',
+      text: 'This app doesn’t need to be downloaded, it’s working online.',
+      image: 'features-one',
+    },
+    {
+      title: 'Multiplatforms',
+      text: ' This app can work on desktop, and on smartphones.',
+      image: 'FeaturesSecond',
+    },
+    {
+      title: 'Free',
+      text: 'This app are free to use, now and forever',
+      image: 'FeaturesThree',
+    },
+  ]
 }
 </script>
