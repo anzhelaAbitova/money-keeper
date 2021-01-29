52b129e96bb1376d45c118963f10e5a397e8f45e
Talai Sergei <tsa@tut.by>
2021-01-29 18:13:21 +0300

    feat: add image card delete and change image

--- frontend/src/components/cabinet/ImageCard.vue
+++ frontend/src/components/cabinet/ImageCard.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="image-card">
-    <div class="image-card__head">
+    <form class="image-card__head">
       <label class="image-card__button" title="change avatar">
         <IconEdit />
-        <input type="file" name="img" accept="image/*" @change="upload">
+        <input type="file" name="img" accept="image/*" @change="upload" :key="uploads">
       </label>
-    </div>
+      <div class="image-card__button" title="delete avatar">
+        <a href="javascript:void(0);" @click.prevent="deleteAvatar">
+          <IconDelete />
+        </a>
+      </div>
+    </form>
     <div class="image-card__body">
       <div class="image-card__image">
         <div v-if="!getUserAvatar" class="image-card__image-default">
           <IconUser />
         </div>
-        <img v-else :src="getUserAvatar" alt="User avatar">
+        <img v-else :src="getUserAvatar" alt="User avatar" width="274" height="246">
       </div>
       <div class="image-card__name">{{ getUserName }}</div>
       <div class="image-card__description">Web-designer</div>
@@ -23,6 +28,7 @@
 import { Component, Vue } from 'vue-property-decorator';
 import IconEdit from '@/components/images-svg/icons/IconEdit.vue';
 import IconUser from '@/components/images-svg/icons/IconUser.vue';
+import IconDelete from '@/components/images-svg/icons/IconDelete.vue';
 import { namespace } from 'vuex-class';
 
 const User = namespace('user');
@@ -31,6 +37,7 @@ const User = namespace('user');
   components: {
     IconEdit,
     IconUser,
+    IconDelete,
   },
 })
 
@@ -41,22 +48,40 @@ export default class ImageCard extends Vue {
 
   @User.Action private setUserAvatar!: (path: string) => void;
 
-  private upload(event: Event): void {
+  private uploads = 0;
+
+  private async upload(event: Event) {
     const target = event.target as HTMLInputElement;
     const file: File = (target.files as FileList)[0];
     const formData = new FormData();
     formData.append('user', this.getUserName);
     formData.append('file', file);
 
-    fetch('https://beinweb.ru/api/save-image.php', {
+    if (this.getUserAvatar !== '') await this.deleteAvatar();
+
+    await fetch('https://beinweb.ru/api/save-image.php', {
       method: 'POST',
       body: formData,
     })
       .then((response) => response.json())
       .then((res) => {
         this.setUserAvatar(`https://beinweb.ru/api/images/${res[0].dir}/${file.name}`);
+        this.uploads += 1;
       })
       .catch((error) => console.log('upload error', error));
+  }
+
+  private async deleteAvatar() {
+    const src = this.getUserAvatar.replace('https://beinweb.ru/api/images/', '');
+    const formData = new FormData();
+    formData.append('dir', src);
+    await fetch('https://beinweb.ru/api/delete-image.php', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then(() => this.setUserAvatar(''))
+      .catch((error) => console.log(error));
   }
 }
 </script>
